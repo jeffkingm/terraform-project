3fe9b4b6ec01cdf56d7f85ba45187b6d377d7e8d
jeffkingm <jffking0@gmail.com>
2024-02-23 01:18:53 +0100

    Edit values in main file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "" # Your value here
-  secret_key = "" # Your value here
+  access_key = "********"
+  secret_key = "**********************"
 }
 
 # 1. Create VPC 
